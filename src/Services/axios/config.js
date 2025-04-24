fee00a0d41d5fb56b51a4d26fad62adb0cc03411
tien nguyen vi <nguyevitien2003@gmail.com>
2025-04-25 01:53:18 +0700

    [Fix] port

--- src/Services/axios/config.js
+++ src/Services/axios/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5262/',
+  baseURL: 'https://opal.io.vn/',
 });
 
 axiosClient.interceptors.request.use(
